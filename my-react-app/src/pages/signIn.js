5924fdf8015bb44423d4352b111ba7060aeedd36
shenzor <toppan.frederic@gmail.com>
2023-11-11 19:07:13 +0100

    user.js ok toutes les maquettes sont implémentées en React App

--- my-react-app/src/pages/signIn.js
+++ my-react-app/src/pages/signIn.js
@@ -1,7 +1,15 @@
 // SignIn.js
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useNavigate } from 'react-router-dom';
 const SignIn = () => {
+  const navigate = useNavigate();
+
+  const handleSignIn = () => {
+    // On effectuez ici les opérations de connexion ou toute autre logique nécessaire
+    // Une fois connecté, on navigue vers la page User
+    navigate('/user');
+  };
   return (
     <main className="main bg-dark">
       <section className="sign-in-content">
@@ -20,13 +28,10 @@ const SignIn = () => {
             <input type="checkbox" id="remember-me" />
             <label htmlFor="remember-me">Remember me</label>
           </div>
-          {/* Placeholder due to static site */}
-          <a href="./user.html" className="sign-in-button">
+          <a onClick={handleSignIn} className="sign-in-button">
             Sign In
           </a>
-          {/* Should be the button below */}
-          {/* <button className="sign-in-button">Sign In</button> */}
-        </form>
+          </form>
       </section>
     </main>
   );

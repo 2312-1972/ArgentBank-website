082cc6999e357d592ad93be4cb301ea398dc8d19
shenzor <toppan.frederic@gmail.com>
2023-11-11 17:22:40 +0100

    routes entouré du Header et du Footer

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -3,17 +3,23 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/home';
 import User from './pages/user';
 import SignIn from './pages/signIn';
+import Header from './components/header';
+import Footer from './components/footer';
 import './main.css';
 
-const App = () => {
+function App  ()  {
   return (
+    
     <Router>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/signin" element={<SignIn />} />
         <Route path="/user" element={<User />} />
       </Routes>
+      <Footer />
     </Router>
+    
   );
 };
 
--- my-react-app/src/pages/home.js
+++ my-react-app/src/pages/home.js
@@ -1,7 +1,7 @@
 import React from "react";
 //import { Link } from "react-router-dom";
-import Header from '../components/header';
-import Footer from '../components/footer';
+//import Header from '../components/header';
+//import Footer from '../components/footer';
 import Hero from '../components/Hero';
 import Features from '../components/Features';
 import '../main.css';
@@ -9,12 +9,12 @@ import '../main.css';
 const Home = () => {
   return (
     <div >
-    <Header />
+    
     <main>
       <Hero />
       <Features />
     </main>
-    <Footer />
+    
   </div>
   );
 };

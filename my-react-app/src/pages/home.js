b44093d7e88292cfde912f9f08aa7d7d3ecb9813
shenzor <toppan.frederic@gmail.com>
2023-11-11 18:31:58 +0100

    SignIn pages Ok

--- my-react-app/src/pages/home.js
+++ my-react-app/src/pages/home.js
@@ -1,7 +1,6 @@
 import React from "react";
 //import { Link } from "react-router-dom";
-//import Header from '../components/header';
-//import Footer from '../components/footer';
+
 import Hero from '../components/Hero';
 import Features from '../components/Features';
 import '../main.css';
@@ -9,7 +8,6 @@ import '../main.css';
 const Home = () => {
   return (
     <div >
-    
     <main>
       <Hero />
       <Features />
--- my-react-app/src/pages/signIn.js
+++ my-react-app/src/pages/signIn.js
@@ -1,11 +1,34 @@
-import React from 'react';
-
+// SignIn.js
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 const SignIn = () => {
   return (
-    <div>
-      <h1>SignIn</h1>
-      {/*  contenu de la page signin */}
-    </div>
+    <main className="main bg-dark">
+      <section className="sign-in-content">
+        <FontAwesomeIcon icon="circle-user" />
+        <h1>Sign In</h1>
+        <form>
+          <div className="input-wrapper">
+            <label htmlFor="username">Username</label>
+            <input type="text" id="username" />
+          </div>
+          <div className="input-wrapper">
+            <label htmlFor="password">Password</label>
+            <input type="password" id="password" />
+          </div>
+          <div className="input-remember">
+            <input type="checkbox" id="remember-me" />
+            <label htmlFor="remember-me">Remember me</label>
+          </div>
+          {/* Placeholder due to static site */}
+          <a href="./user.html" className="sign-in-button">
+            Sign In
+          </a>
+          {/* Should be the button below */}
+          {/* <button className="sign-in-button">Sign In</button> */}
+        </form>
+      </section>
+    </main>
   );
 };
 

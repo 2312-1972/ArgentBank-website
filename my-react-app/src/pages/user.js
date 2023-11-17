68d6049adadfe7bbc7b2bba82437e1f246ac17ce
shenzor <toppan.frederic@gmail.com>
2023-11-17 17:22:50 +0100

    ajout lastName dans user.js

--- my-react-app/src/pages/user.js
+++ my-react-app/src/pages/user.js
@@ -15,7 +15,7 @@ const User = ({ isAuthenticated, user, getUser }) => {
       {isAuthenticated ? (
         <div>
           <div className="header">
-            <h1>Welcome back<br />{user ? user.firstName : 'User'}!</h1>
+            <h1>Welcome back<br />{user ? `${user.firstName} ${user.lastName}` : 'User'}!</h1>
             <button className="edit-button">Edit Name</button>
           </div>
           <h2 className="sr-only">Accounts</h2>

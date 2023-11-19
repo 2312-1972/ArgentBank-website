a72a55482076a32a5df5cc5cbbb7bd17d7a9ad8a
shenzor <toppan.frederic@gmail.com>
2023-11-19 11:59:19 +0100

    headerEdit ok

--- my-react-app/src/components/HeaderEdit.js
+++ my-react-app/src/components/HeaderEdit.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import argentBankLogo from "../img/argentBankLogo.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fas } from "@fortawesome/free-solid-svg-icons";
 import { useSelector } from "react-redux";
-import { selectIsConnected, selectUserFirstName, selectUserLastname } from "../store/selectors";
+import {
+  selectIsConnected,
+  selectUserFirstName,
+  selectUserLastname,
+} from "../store/selectors";
 import { useDispatch } from "react-redux";
 import { signOut } from "../actions/user.action";
 
@@ -14,7 +17,7 @@ library.add(fas);
 const HeaderEdit = () => {
   const isConnected = useSelector(selectIsConnected);
   const userFirstName = useSelector(selectUserFirstName);
-  const userLastName = useSelector( selectUserLastname );
+  const userLastName = useSelector(selectUserLastname);
   const dispatch = useDispatch();
 
   const handleSignOut = () => {
@@ -23,37 +26,66 @@ const HeaderEdit = () => {
 
   return (
     <nav className="main-nav">
-      <Link to="/" className="main-nav-logo">
-        <img
-          className="main-nav-logo-image"
-          src={argentBankLogo}
-          alt="Argent Bank Logo"
+      <div style={{ display: "flex" }}>
+        <FontAwesomeIcon
+          icon="fa-solid fa-vault"
+          style={{
+            fontSize: "30px",
+            color: " #00bc77",
+            paddingBottom: "4px",
+            marginTop: "20px",
+            paddingLeft: "20px",
+          }}
         />
-        <h1 className="sr-only">Argent Bank</h1>
-      </Link>
+        <h1
+          style={{
+            fontSize: "20px",
+            color: " #00bc77",
+            marginTop: "20px",
+            paddingLeft: "10px",
+          }}
+        >
+          Argent Bank
+        </h1>
+      </div>
       <div>
         {isConnected ? (
           <>
-            
-            <Link to="/user" className="main-nav-item">
-            <span style={{ fontSize: "16px", color: "#00bc77"}}>
-            {`${userFirstName} - ${userLastName}`}      </span>
+            <span
+              style={{
+                fontSize: "16px",
+                color: "#00bc77",
+                textDecoration: "none",
+              }}
+            >
+              {`${userFirstName} - ${userLastName}`}{" "}
+            </span>
             &nbsp;&nbsp;
+            <Link to="/user" className="main-nav-item">
               <FontAwesomeIcon
                 icon={["fas", "user-circle"]}
                 style={{ fontSize: "30px", color: " #00bc77" }}
               />
             </Link>
             &nbsp;&nbsp;&nbsp;&nbsp;
+            <FontAwesomeIcon
+              icon="fa-solid fa-gear"
+              style={{
+                fontSize: "23px",
+                color: " #00bc77",
+                paddingBottom: "4px",
+                marginTop: "20px",
+              }}
+            />
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
             <Link to="/" className="main-nav-item" onClick={handleSignOut}>
               <FontAwesomeIcon
-                icon="fa-solid fa-gear"
-                style={{ fontSize: "23px", color: " #00bc77", paddingBottom: "4px"}}
-              />
-              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-              <FontAwesomeIcon
                 icon={["fas", "power-off"]}
-                style={{ fontSize: "23px", color: " #00bc77", paddingBottom: "4px" }}
+                style={{
+                  fontSize: "23px",
+                  color: " #00bc77",
+                  paddingBottom: "4px",
+                }}
               />
             </Link>
           </>
